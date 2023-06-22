73e8a2d598b1870290f476e757686ff36e1b0c3d
Ruslan <ruslan.mirets@gmail.com>
2023-06-22 10:55:43 +0300

    Init

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</>
+	);
+}
